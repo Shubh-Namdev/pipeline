3115ed87f7cf835ba1b26f99a01bdf6450a742d2
Shubham Namdev <s.namdev013@gmail.com>
2025-10-08 12:29:14 +0530

    jenkins, k8s, docker learnings added

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:21-jdk-alpine
-VOLUME /tmp
-COPY build/libs/springboot-mysql-k8s-0.0.1-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+WORKDIR /app
+COPY build/libs/k8s-pipeline-0.0.1-SNAPSHOT.jar app.jar
+ENTRYPOINT ["java","-jar","/app/app.jar"]
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,13 +3,20 @@ package com.example.demo.controller;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<String> greet() {
+        return ResponseEntity.ok().body("Hello, User!!!");
+    }
 
     @PostMapping("/{message}")
     public User createUser(@PathVariable String message) {
